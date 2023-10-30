c10af8193769180f3d8d97455738d428a77db31e
Daveroyz <davidsewu65@gmail.com>
2023-10-30 03:44:15 -0700

    mode

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -70,6 +70,7 @@ test_db_storage.py'])
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
+    
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
@@ -89,7 +90,7 @@ class TestFileStorage(unittest.TestCase):
 
 
     class TestDBStorage(unittest.TestCase):
-    """Test the DBStorage class"""
+        """Test the DBStorage class"""
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
                      "not testing db storage")
