855d2339ebf51bc7b44cf5d8b4ff468d7c914a7e
craymahdee142 <craymahdaniel@gamil.com>
2023-10-30 21:51:01 +0000

    updated test cases

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -70,7 +70,7 @@ test_db_storage.py'])
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class"""
-    
+
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_all_returns_dict(self):
         """Test that all returns a dictionaty"""
@@ -87,7 +87,6 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
-
 
     class TestDBStorage(unittest.TestCase):
         """Test the DBStorage class"""
